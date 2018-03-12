f68b00ea7b0aee37f5519aed7a393635689c9a34
benjimon24 <benji102491@gmail.com>
2018-03-12 00:48:42 -0400

    no pun intended

--- src/Main.java
+++ src/Main.java
@@ -62,7 +62,7 @@ public class Main extends PApplet{
                         noStroke();
                     }
                     ellipse(orb.getPosX(),orb.getPosY(), boardObj.getOrbSize(), boardObj.getOrbSize());
-                    noStroke();
+                    noStroke(); //otherwise the whole board has a stroke
                 }
             }
 
